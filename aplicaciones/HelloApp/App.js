3004cfcb0230a02f74ff95c08143b4b8574faeed
dragonnomada <dragonnomada123@gmail.com>
2021-08-09 11:30:15 -0500

    Se creó el proyecto HelloApp

---
+++ aplicaciones/HelloApp/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hola React Native :O</Text>
+    </View>
+  )
+}
