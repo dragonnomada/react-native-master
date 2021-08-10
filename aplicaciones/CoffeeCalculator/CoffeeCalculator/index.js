35241306ddfe8ef7f3624cd2c3f1b647fc171519
dragonnomada <dragonnomada123@gmail.com>
2021-08-10 18:33:35 -0500

    Se quitaron dependencias innecesarias

--- aplicaciones/CoffeeCalculator/CoffeeCalculator/index.js
+++ aplicaciones/CoffeeCalculator/CoffeeCalculator/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View } from "react-native";
 import Banner from "./Banner";
 import CoffeeButton from "./CoffeeButton";
