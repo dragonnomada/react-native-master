27aba10bbce02f42d5412786138d4f02299ad53d
dragonnomada <dragonnomada123@gmail.com>
2021-08-10 16:26:53 -0500

    Se actualizó la imagen del Banner

--- aplicaciones/CoffeeCalculator/CoffeeCalculator/Banner.js
+++ aplicaciones/CoffeeCalculator/CoffeeCalculator/Banner.js
@@ -6,7 +6,7 @@ export default function Banner() {
         <View>
             <Image
                 source={{
-                    uri: "https://placekitten.com/600",
+                    uri: "https://images.unsplash.com/photo-1512568400610-62da28bc8a13?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
                     width: 600,
                     height: 200
                 }}
