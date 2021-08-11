5962885c5c86c46c53dec3f042d444124b823156
dragonnomada <dragonnomada123@gmail.com>
2021-08-11 11:39:22 -0500

    Se actualizó para usar Pull to Refresh

--- aplicaciones/SuperUser/RandomUserList/v1.js
+++ aplicaciones/SuperUser/RandomUserList/v1.js
@@ -1,46 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, FlatList, Image, SafeAreaView, Text, View } from "react-native";
 
 function useRandomUserList() {
     const [users, setUsers] = useState([]);
     const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
 
     return {
         users,
         error,
-        loading,
         async getUsers() {
-            setError("");
-            setLoading(true);
-
             const response = await fetch("https://randomuser.me/api?results=100&nat=us,gb");
-
-            // setError("Falló la obtención de usuarios");
-            // return;
 
             if (!response.ok) {
                 // TODO: Notificar que el servidor falló
                 const error = await response.text();
                 setError(`${error} (${response.status} / ${response.statusText})`);
-                setLoading(false);
                 return;
             }
 
             const data = await response.json();
 
             setUsers(data.results);
-            setLoading(false);
         }
     };
 }
 
 export default function RandomUserList() {
-    const { users, error, loading, getUsers } = useRandomUserList();
-
-    useEffect(() => {
-        getUsers();
-    }, []);
+    const { users, error, getUsers } = useRandomUserList();
 
     return (
         <View>
@@ -52,20 +38,12 @@ export default function RandomUserList() {
                     getUsers()
                 }} />
             </View>
-            {
-                error ? (
-                    <View style={{ backgroundColor: "crimson", padding: 12, elevation: 5 }}>
-                        <Text style={{ color: "white" }}>{error}</Text>
-                    </View>
-                ) : null
-            }
+            <View>
+                <Text>{error}</Text>
+            </View>
             <SafeAreaView>
                 <FlatList
                     data={users}
-                    refreshing={loading}
-                    onRefresh={() => {
-                        getUsers();
-                    }}
                     renderItem={({ item: user }) => (
                         <View
                             style={{
